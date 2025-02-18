9c55164f357ca7b6517c86230fe0ed8498c4f309
Fernando Mejia Laguna <mejia190899@gmail.com>
2025-02-17 21:34:09 -0600

    Added links to navbar.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 // main.js
 import "./countdown.js";
 import "./carousel.js";
-import "./header_sticky.js"; // Asegúrate de que la ruta sea correcta
+import "./header_sticky.js";
+import "./navbar-toggle.js";
