a078088019484bf6f52dd443d9a3c563b3a391fb
Fernando Mejia Laguna <mejia190899@gmail.com>
2025-03-13 21:35:45 -0600

    Fixed carroussel.

--- js/carousel.js
+++ js/carousel.js
@@ -1,9 +1,30 @@
-const carrete = document.getElementById("carrete");
+let carrete = document.getElementById("carrete");
+let animationFrame;
 
-function scrollLeft() {
-  carrete.scrollBy({ left: -300, behavior: "smooth" });
+function resetAnimation() {
+  carrete.style.animation = "none";
+  // Forzar reflow
+  void carrete.offsetWidth;
+  carrete.style.animation = "scroll 15s linear infinite";
 }
 
-function scrollRight() {
-  carrete.scrollBy({ left: 300, behavior: "smooth" });
+// Optimizar redimensionamiento con debounce
+function debounce(func, timeout = 100) {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func.apply(this, args);
+    }, timeout);
+  };
 }
+
+const handleResize = debounce(() => {
+  cancelAnimationFrame(animationFrame);
+  animationFrame = requestAnimationFrame(resetAnimation);
+});
+
+window.addEventListener("resize", handleResize);
+
+// Inicializar
+resetAnimation();
